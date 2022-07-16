89e7773506b0d5931d6d1460d65d240d6e053229
unknown <engraaadeyemi@gmail.com>
2022-07-16 11:33:11 +0100

    done 100%

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,50 @@ import { useGlobalContext } from "../utils/context";
 
 const Footer = () => {
   const { darkMode } = useGlobalContext();
-
   return (
-    <Footer>
-      {!darkMode ? (
-        <div className="dark-element">Footer</div>
+    <section>
+      {darkMode ? (
+        <div className=" flex flex-col justify-center items-center gap-2 py-3 bg-white">
+          <p className="text-xl">
+            Challenge by{" "}
+            <span className="text-red-500 font-bold underline">
+              Front end Mentor
+            </span>
+          </p>
+          <p className="text-xl">
+            Made with love by{" "}
+            <a
+              href="https://github.com/Shurrd"
+              target="_blank"
+              rel="noreferrer"
+              className="text-red-500 font-semibold underline"
+            >
+              Abraham
+            </a>
+          </p>
+        </div>
       ) : (
-        <div>Footer</div>
+        <div className=" flex flex-col justify-center items-center gap-2 dark-element py-3">
+          <p className="text-xl text-white">
+            Challenge by{" "}
+            <span className="text-red-200 font-bold underline">
+              Front end Mentor
+            </span>
+          </p>
+          <p className="text-xl text-white">
+            Made with love by{" "}
+            <a
+              href="https://github.com/Shurrd"
+              target="_blank"
+              rel="noreferrer"
+              className="text-red-200 font-semibold underline"
+            >
+              Abraham
+            </a>
+          </p>
+        </div>
       )}
-    </Footer>
+    </section>
   );
 };
 
